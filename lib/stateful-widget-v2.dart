0b31fe0a80ce5c48b4f12e242b646d1e7aff4f60
Juan Camilo Ospina <juan.ospina.quintero@gmail.com>
2019-05-24 09:17:17 -0500

    Stateful widget example

--- lib/stateful-widget-v2.dart
+++ lib/stateful-widget-v2.dart
@@ -10,7 +10,6 @@ void main() {
 class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
         appBar: AppBar(
           leading: IconButton(
@@ -28,7 +27,7 @@ class TutorialHome extends StatelessWidget {
           ],
         ),
         body: Center(
-            child: Counter(),
+          child: Counter(),
         ),
         floatingActionButton: FloatingActionButton(
             tooltip: 'Add',
@@ -37,15 +36,37 @@ class TutorialHome extends StatelessWidget {
         )
     );
   }
+
+}
+
+class CounterDisplay extends StatelessWidget{
+  CounterDisplay({this.count});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: onPressed,
+      child: Text('Increment')
+    );
+  }
 }
 
 class Counter extends StatefulWidget {
-  // This class is the configuration for the state. It holds the
-  // values (in this case nothing) provided by the parent and used by the build
-  // method of the state. Fields in a Widget subclass are always marked "final"
-
   @override
-  _CounterState createState() => _CounterState();
+  State<StatefulWidget> createState() => _CounterState();
 }
 
 class _CounterState extends State<Counter> {
@@ -53,24 +74,16 @@ class _CounterState extends State<Counter> {
 
   void _increment() {
     setState(() {
-      // This call to setState tells the Flutter framework that something
-      // has changed in this State, which causes it to rerun the build
-      // method below so that the display can reflect the updated values.
-      // If you change _counter without calling setState(), then the build
-      // method won't be called again and so nothing would appear to happen
-      _counter++;
+      ++_counter;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: <Widget>[
-        RaisedButton(
-          onPressed: _increment,
-          child: Text('Increment')
-        ),
-        Text('Count: $_counter')
+        CounterIncrementor(onPressed: _increment),
+        CounterDisplay(count: _counter)
       ],
     );
   }
